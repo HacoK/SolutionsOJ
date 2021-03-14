8425eac465bbe7ca7ab28b5d162279637d73426c
HacoK <haco9812@gmail.com>
2021-03-14 12:39:30 +0800

    redo DFS

--- 2-3/solution.py
+++ 2-3/solution.py
@@ -1,31 +1,45 @@
-def dfs(graph, visited, cur_node, cur_depth):
-    global max_depth
-    max_depth = max(max_depth,cur_depth)
-    visited[cur_node] = True
-    for next_node in graph[cur_node]:
-        if not visited[next_node]:
-            dfs(graph, visited.copy(), next_node, cur_depth+1)
+from itertools import permutations
 
 if __name__ == '__main__':
     test_cnt = int(input())
     for t in range(test_cnt):
-        n, start_node = input().split(" ")
+        n, init = input().split(' ')
         n = int(n)
+        vertex = input().split(' ')
         graph = {}
-        nodes = input().split(" ")
-        for node in nodes:
-            graph[node] = []
+        visited_init = {}
+        for v in vertex:
+            graph[v] = []
+            visited_init[v] = False
         for i in range(n):
-            row = input().split(" ")
-            node = row[0]
-            table = [int(x) for x in row[1:]]
-            for idx, exist in enumerate(table):
-                if exist == 1:
-                    graph[node].append(nodes[idx])
-        visited = {}
-        for node in nodes:
-            visited[node] = False
-        
+            raw_line = input().split(' ')
+            v = raw_line[0]
+            raw_line = raw_line[1:]
+            for i in range(n):
+                if raw_line[i] == '1':
+                    graph[v].append(vertex[i])
+
         max_depth = 0
-        dfs(graph, visited.copy(), start_node, 1)
+        stacks = [[init]]
+        visited_co = [visited_init]
+        depths_co = [{init:1}]
+        while len(stacks) != 0:
+            stack = stacks.pop()
+            visited = visited_co.pop()
+            depths = depths_co.pop()
+            max_depth = max(max_depth,max(depths.values()))
+            while len(stack) > 0:
+                v = stack.pop()
+                if not visited[v]:
+                    visited[v] = True
+                    next_v = []
+                    for neighbor in graph[v]:
+                        if not visited[neighbor]:
+                            next_v.append(neighbor)
+                            depths[neighbor] = depths[v] + 1
+                    for neighbors in permutations(next_v):
+                        stacks.append(stack+list(neighbors))
+                        visited_co.append(visited.copy())
+                        depths_co.append(depths)
+                    break
         print(max_depth)
